88ef2573999ff4309495d966fa01a30b755a26b6
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-10-18 11:10:37 +0530

    Update gateway index to include new payment gateways
    - Add imports for Cashfree, Square, and PayU gateways
    - Extend PaymentGateway.initialize method to support new gateways

--- js-sdk/src/gateways/index.js
+++ js-sdk/src/gateways/index.js
@@ -1,5 +1,9 @@
 import StripeGateway from './stripe.js';
 import RazorpayGateway from './razorpay.js';
+import BraintreeGateway from './braintree.js';
+import CashfreeGateway from './cashfree.js';
+import SquareGateway from './square.js';
+import PayUGateway from './payu.js';
 import { UniPayError } from '../errors.js';
 
 export const PaymentGateway = {
@@ -9,6 +13,14 @@ export const PaymentGateway = {
         return new StripeGateway(credentials);
       case 'razorpay':
         return new RazorpayGateway(credentials);
+      case 'braintree':
+        return new BraintreeGateway(credentials);
+      case 'cashfree':
+        return new CashfreeGateway(credentials);
+      case 'square':
+        return new SquareGateway(credentials);
+      case 'payu':
+        return new PayUGateway(credentials);
       default:
         throw new UniPayError(`Unsupported gateway: ${gatewayName}`);
     }
